0a26245a273a6195f59b71b694314f31f0de616d
Jan Range <30547301+JR-1991@users.noreply.github.com>
2025-04-02 10:54:13 +0200

    update dist

--- dist/example.sh
+++ dist/example.sh
@@ -7,5 +7,8 @@
 # With BLOSUM62 and fraction 0.035
 ./aligner test_input.json -o output_blosum62.tsv -f 0.035 -s blosum62
 
+# With BLOSUM62 and fraction 0.035 and minimum 10 matches
+./aligner test_input.json -f 0.035 -s blosum62 -m 10
+
 # With identity scoring and fraction 0.035
 ./aligner test_input.json -o output_identity.tsv -f 0.035 -s identity
